dd026ddd6c0009b4915510355bb17134ce4724e3
J <32362931+jm1997@users.noreply.github.com>
2018-03-05 10:54:23 +0000

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,25 @@
+var feedURL = "your_API_url_goes_here";
+
+$(document).on('pagecreate', '#feedPage', function(event) {
+	
+	
+	<!-- Use an HTML GET request to obtain data from an API  -->
+	var xmlhttp=new XMLHttpRequest();
+	xmlhttp.open("GET", feedURL, false);
+	xmlhttp.send();
+		
+		
+	<!-- parse the resulting JSON into Javascript Data Object -->
+	<!-- you can use a live parser to inspect the contents of the JSON
+	<!-- http://json.parser.online.fr/ -->
+	var weather= JSON.parse(xmlhttp.responseText);
+	
+	
+	<!-- Define Ractive binding -->
+	var ractive = new Ractive({
+    	el: 'container', <!-- where -->
+    	template: '#myTemplate', <!-- how -->
+    	data: { weather : weather.consolidated_weather } <!-- what - specify the list of weather reports using dot notation-->
+	});
+	
+});
